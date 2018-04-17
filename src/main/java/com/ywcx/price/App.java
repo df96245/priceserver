a6be223fd49d8a3c6ff4feec23876ad8f54dd316
df96245 <363997431@qq.com>
2018-04-17 01:08:22 +0000

    Update App.java
    remove import from app.java

--- src/main/java/com/ywcx/price/App.java
+++ src/main/java/com/ywcx/price/App.java
@@ -1,17 +1,5 @@
 package com.ywcx.price;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-
-import org.apache.http.message.BasicNameValuePair;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-
-import com.ywcx.price.test.MapDistanceUtil;
-import com.ywcx.price.test.Point;
-import com.ywcx.price.test.RandomUtil;
 import com.ywcx.price.util.HttpUtil;
 
 /**
